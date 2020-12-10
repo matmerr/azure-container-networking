be30125edd02b0251c3cef3b7821e7a0c9560838
Mathew Merrick <matmerr@microsoft.com>
2020-12-10 12:04:37 -0800

    fix: acncnli move to hack

--- hack/acncli/installer/install.go
+++ hack/acncli/installer/install.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strings"
 
-	c "github.com/Azure/azure-container-networking/acncli/api"
+	c "github.com/Azure/azure-container-networking/hack/acncli/api"
 )
 
 type InstallerConfig struct {
